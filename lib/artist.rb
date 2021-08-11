62a131b76e370df06bdc60cdf3464fb341aa2f1f
vincentbaylon <vincentbaylon@gmail.com>
2021-08-11 10:54:22 -0700

    commit tree 10d656e55e2a4bef4aa49b8636b34acd83f6236a

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,13 @@
 require 'pry'
+require_relative './concerns/memorable'
+require_relative './concerns/findable'
+require_relative './concerns/paramable'
 
 class Artist
+  extend Memorable
+  extend Findable
+  include Paramable
+  
   attr_accessor :name
   attr_reader :songs
 
@@ -11,20 +18,8 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -34,9 +29,5 @@ class Artist
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 end
